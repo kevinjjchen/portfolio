e86f5dfa3ef46a61ac62b7b9152daf9defba2a53
kevinjjchen <kevinjj@umich.edu>
2023-07-09 22:18:05 -0700

    sunday

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,14 @@
-import { motion } from 'framer-motion'
-import { styles } from '../styles'
-import { ComputersCanvas } from './canvas'
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
-      <div className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
@@ -13,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Kevin</span>
+            Hi, I'm <span className='text-[#915EFF]'>Kevin Chen</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I do stuff <br className='sm:block hidden' />
@@ -41,9 +44,8 @@ const Hero = () => {
           </div>
         </a>
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
